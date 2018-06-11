0c1fa378024bc2823a64a7957bc4cb59eef36dae
Chasen Arey <ccarey@ncsu.edu>
2018-06-11 13:58:55 -0600

    ex 4

--- jsdrills/drill1.js
+++ jsdrills/drill1.js
@@ -1,7 +1,9 @@
-function whoAmI(name,age){
-    
+function whoAmI(name,age=30){
+    let yearOfBirth = 2018-age;
     console.log('Hi, my name is Donny and I\'m 24 years old');
+    console.log(`I was born in ${yearOfBirth}`);
 }
+
 
 whoAmI();
 
