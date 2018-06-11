b355803616513b66eccd05886353ffbe3a9c7e2e
Jonny Cheng <jcheng02@mica.edu>
2018-06-11 17:10:13 -0400

    jedi name

---
+++ jsdrills/drills2.js
@@ -0,0 +1,9 @@
+function jediName (firstName='Beyonce',lastName='Knowles'){
+
+let sliceThree=lastName.slice(0,3);
+let sliceTwo=firstName.slice(0,2);
+return sliceThree+sliceTwo
+
+}
+
+console.log(jediName());
