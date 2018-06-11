65b900ade7325b6ea809ee21e68bb6335c047c20
Chasen Arey <ccarey@ncsu.edu>
2018-06-11 16:33:13 -0600

    month ex.

--- jsdrills/drills2.js
+++ jsdrills/drills2.js
@@ -24,44 +24,74 @@
 
 // beyond(0);
 
-function decode(word){
+// function decode(word){
 
-let decodedLetter
+// let decodedLetter
 
-    if (word[0] === 'a'){
-    decodedLetter=word[1];
-    }
-    else if (word[0] === 'b'){
-        decodedLetter=word[2];
-    }
-    else if (word[0] === 'c'){
-        decodedLetter=word[3];
-    }
-    else if (word[0] === 'd'){
-        decodedLetter=word[4];
-    }
-    else{
-        decodedLetter=' ';
-    }
+//     if (word[0] === 'a'){
+//     decodedLetter=word[1];
+//     }
+//     else if (word[0] === 'b'){
+//         decodedLetter=word[2];
+//     }
+//     else if (word[0] === 'c'){
+//         decodedLetter=word[3];
+//     }
+//     else if (word[0] === 'd'){
+//         decodedLetter=word[4];
+//     }
+//     else{
+//         decodedLetter=' ';
+//     }
 
-    return decodedLetter;
-}
+//     return decodedLetter;
+// }
 
 
-function decodeSentence(sentence){
+// function decodeSentence(sentence){
 
-let wordArray = sentence.split(' ');
-let decodedMessage=" ";
+// let wordArray = sentence.split(' ');
+// let decodedMessage=" ";
   
-    for (let i=0;i<wordArray.length;i++){
-        decodedMessage += decode(wordArray[i]); 
+//     for (let i=0;i<wordArray.length;i++){
+//         decodedMessage += decode(wordArray[i]); 
         
-    }
-    console.log(decodedMessage);
+//     }
+//     console.log(decodedMessage);
 
+// }
+
+
+// decodeSentence('craft block argon meter bells brown croon droop');
+
+// //decode('apple');
+
+function daysInMonth(month, leapYear){
+    switch(month, leapYear){
+        case 'Jan':
+        case 'Mar':
+        case 'May':
+        case 'July':
+        case 'Aug':
+        case 'Oct':
+        case 'Dec':
+            result = month+` has 31 days`
+            break;
+        case 'April':
+        case 'June':
+        case 'Sept':
+        case 'Nov':
+            result = `${month} has 30 days`;
+        case 'Feb':
+            if(leapYear === true){
+                return `${month} has 29 days`
+            }else{
+                return`${month} has 28 days`
+            }
+            break;
+        default:
+            throw 'Must provide a valid month.'
+    console.log('month');        
+    }   
 }
-
-
-decodeSentence('craft block argon meter bells brown croon droop');
-
-//decode('apple');
+daysInMonth('Jan', true);
