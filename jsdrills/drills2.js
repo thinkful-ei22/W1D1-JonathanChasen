5b7d8ea72ae96ae8ab8665a640607621000cec5f
Jonny Cheng <jcheng02@mica.edu>
2018-06-11 17:54:03 -0400

    cracking the code

--- jsdrills/drills2.js
+++ jsdrills/drills2.js
@@ -8,18 +8,60 @@
 
 // console.log(jediName());
 
-function beyond(num){
-    if (num === Infinity || num === -Infinity){
-        console.log('And beyond');
+// function beyond(num){
+//     if (num === Infinity || num === -Infinity){
+//         console.log('And beyond');
+//     }
+//     else if (num > 0){
+//         console.log('To infinity');
+//     }
+//     else if (num < 0){
+//         console.log('To negative infinity');
+//     }else{
+//         console.log('Staying home');
+//     }
+// }
+
+// beyond(0);
+
+function decode(word){
+
+let decodedLetter
+
+    if (word[0] === 'a'){
+    decodedLetter=word[1];
     }
-    else if (num > 0){
-        console.log('To infinity');
+    else if (word[0] === 'b'){
+        decodedLetter=word[2];
     }
-    else if (num < 0){
-        console.log('To negative infinity');
-    }else{
-        console.log('Staying home');
+    else if (word[0] === 'c'){
+        decodedLetter=word[3];
     }
+    else if (word[0] === 'd'){
+        decodedLetter=word[4];
+    }
+    else{
+        decodedLetter=' ';
+    }
+
+    return decodedLetter;
 }
 
-beyond(0);
+
+function decodeSentence(sentence){
+
+let wordArray = sentence.split(' ');
+let decodedMessage=" ";
+  
+    for (let i=0;i<wordArray.length;i++){
+        decodedMessage += decode(wordArray[i]); 
+        
+    }
+    console.log(decodedMessage);
+
+}
+
+
+decodeSentence('craft block argon meter bells brown croon droop');
+
+//decode('apple');
