ff6b59d6d563ad0d60f187d544adddd9c3424e03
Chasen Arey <ccarey@ncsu.edu>
2018-06-11 17:36:57 -0600

    rock paper sciss

--- jsdrills/drills2.js
+++ jsdrills/drills2.js
@@ -67,40 +67,56 @@
 // //decode('apple');
 
 
-function daysInMonth(month,leapYear=false){
-    let text;
-    switch (month) {
+// function daysInMonth(month,leapYear=false){
+//     let text;
+//     switch (month) {
         
-        case 'January':
-        case 'March':
-        case 'May':
-        case 'July':
-        case 'August':
-        case 'October':
-        case 'December':
-          text=`${month} has 31 days`;  
-        break;
-        case 'April':
-        case 'June':
-        case 'September':
-        case 'November':
-          text=`${month} has 30 days'`;
-        break;
-        case 'February':
-            if(leapYear === true){
-                    text=`${month} has 29 days`;
-                  }else{
-                     text=`${month} has 28 days`;
-                  }
-            break;
-      default:
-        console.log('Must provide a valid month.');
-    }
-         return text;
-    }
+//         case 'January':
+//         case 'March':
+//         case 'May':
+//         case 'July':
+//         case 'August':
+//         case 'October':
+//         case 'December':
+//           text=`${month} has 31 days`;  
+//         break;
+//         case 'April':
+//         case 'June':
+//         case 'September':
+//         case 'November':
+//           text=`${month} has 30 days'`;
+//         break;
+//         case 'February':
+//             if(leapYear === true){
+//                     text=`${month} has 29 days`;
+//                   }else{
+//                      text=`${month} has 28 days`;
+//                   }
+//             break;
+//       default:
+//         console.log('Must provide a valid month.');
+//     }
+//          return text;
+//     }
     
-    console.log(daysInMonth('February',true));
+//     console.log(daysInMonth('February',true));
   
   
-  
-  
+function rockPaperSissors(num){
+    const randomNo = Math.floor(Math.random() * 3) + 1;
+
+    if(num<1 || num >3 ){
+        throw 'error: number has to be between 1 and 3'
+    }else if(num === randomNo){
+        return 'tie';
+    }else if(num === 1 && randomNo ===3 ){
+        return 'You win! Rock beats Scissors';
+    }else if(num === 3 && randomNo ===2 ){
+        return 'You win! Scissors beats Paper';
+    }else if(num === 2 && randomNo ===1){
+        return 'You win! Paper beats Rock';
+    }else{
+        return 'Sorry, You Lose';
+    }
+}
+console.log(rockPaperSissors(1));
