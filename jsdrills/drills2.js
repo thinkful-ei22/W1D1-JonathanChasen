f840deded40a844cb0c4de96ca6af26b5c57a63e
Chasen Arey <ccarey@ncsu.edu>
2018-06-11 15:21:34 -0600

    infinity

--- jsdrills/drills2.js
+++ jsdrills/drills2.js
@@ -1,9 +1,25 @@
-function jediName (firstName='Beyonce',lastName='Knowles'){
+// function jediName (firstName='Beyonce',lastName='Knowles'){
 
-let sliceThree=lastName.slice(0,3);
-let sliceTwo=firstName.slice(0,2);
-return sliceThree+sliceTwo
+// let sliceThree=lastName.slice(0,3);
+// let sliceTwo=firstName.slice(0,2);
+// return sliceThree+sliceTwo
 
+// }
+
+// console.log(jediName());
+
+function beyond(num){
+    if (num === Infinity || num === -Infinity){
+        console.log('And beyond');
+    }
+    else if (num > 0){
+        console.log('To infinity');
+    }
+    else if (num < 0){
+        console.log('To negative infinity');
+    }else{
+        console.log('Staying home');
+    }
 }
 
-console.log(jediName());
+beyond(0);
